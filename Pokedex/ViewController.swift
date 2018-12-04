d5888ef2e41cb55aabea41c1feaa336629776278
WHITEOPEN <whiteopen@WHITEOPENs-MacBook-Pro.local>
2018-12-04 13:53:19 +0700

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by WHITEOPEN on 04/12/18.
+//  Copyright © 2018 PT. WhiteOpen Technology. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
